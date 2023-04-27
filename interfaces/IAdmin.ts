a12a0a3155f4075d5fa1b0963b8f25df3fbea180
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2023-04-27 17:17:49 +0300

    Удалил IAdminAuthorization

--- interfaces/IAdmin.ts
+++ interfaces/IAdmin.ts
@@ -1,7 +1,9 @@
+import IAuthorization from "./IAuthorization";
 import ID from "./ID";
 
 interface IAdmin extends ID {
-    firstName: string
+    firstName: string,
+    authorization?: IAuthorization
 }
 
 export default IAdmin;
--- interfaces/IAdminAuthorization.ts
+++
@@ -1,7 +0,0 @@
-import IAdmin from "./IAdmin";
-import IAuthorization from "./IAuthorization";
-
-interface IAdminAuthorization extends IAdmin, IAuthorization {
-}
-
-export default IAdminAuthorization;
