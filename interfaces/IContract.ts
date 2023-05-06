6eefec2931cd8efc69498edeca8a75cb18d1c334
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2023-05-06 20:47:33 +0300

    Добавил поле cheques

--- interfaces/IContract.ts
+++ interfaces/IContract.ts
@@ -1,11 +1,13 @@
 import IBaseContract from "./IBaseContract";
 import IBaseProduct from "./IBaseProduct";
 import IBill from "./IBill";
+import ICheque from "./ICheque";
 import IStatus from "./IStatus";
 
 interface IContract extends IBaseContract, IStatus {
     sellerBill: IBill,
     buyerBill: IBill,
+    cheques: ICheque[],
     products: IBaseProduct[]
 }
 
